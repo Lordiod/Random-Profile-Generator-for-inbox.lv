2f2409e843194eb152fd072ffaf2fd6bceedd4a2
Youssef Yasser <120683871+Lordiod@users.noreply.github.com>
2025-07-14 21:43:40 +0300

    Reorder import statement for lazy loading of GUI in main execution

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.gui import display_profile_in_window
-
 if __name__ == "__main__":
+    # Lazy import - only import GUI when running the app
+    from src.gui import display_profile_in_window
     display_profile_in_window()
